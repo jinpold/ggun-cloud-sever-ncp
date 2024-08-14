c0f6bbb699e59b81120340eaf45f4e3b64451edb
jinpold <jinpold@gmail.com>
2024-08-14 15:29:37 +0900

    240814

--- server/gateway-server/src/main/java/store/ggun/gateway/controller/AuthRouter.java
+++ server/gateway-server/src/main/java/store/ggun/gateway/controller/AuthRouter.java
@@ -10,6 +10,9 @@ import store.ggun.gateway.domain.dto.LoginDto;
 import store.ggun.gateway.service.impl.AuthServiceImpl;
 
 
+
+
+
 @Configuration
 @RequiredArgsConstructor
 public class AuthRouter {
